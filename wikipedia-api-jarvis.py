ea42655f199172e2fd8b0ba25cae2c6a71beb8b5
fredi-python <83492589+fredi-python@users.noreply.github.com>
2021-05-29 22:56:45 +0200

    Add files via upload

---
+++ wikipedia-api-jarvis.py
@@ -0,0 +1,15 @@
+def rec():
+    import speech_recognition as sr
+
+
+    r = sr.Recognizer()
+    with sr.Microphone() as source:
+        print("Speak Anything :")
+        audio = r.listen(source)
+    try:
+        text = r.recognize_google(audio)
+        print("".format(text))
+    except:
+        print("Sorry could not recognize what you said")
+
+rec()
